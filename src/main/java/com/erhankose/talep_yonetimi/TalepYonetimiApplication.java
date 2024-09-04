18500e5114ed6a611caddcda0e1477fc46a8b3f6
erhankose <erhankose01@gmail.com>
2024-09-04 18:22:02 +0300

    ilk commit

---
+++ src/main/java/com/erhankose/talep_yonetimi/TalepYonetimiApplication.java
@@ -0,0 +1,13 @@
+package com.erhankose.talep_yonetimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalepYonetimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalepYonetimiApplication.class, args);
+	}
+
+}
